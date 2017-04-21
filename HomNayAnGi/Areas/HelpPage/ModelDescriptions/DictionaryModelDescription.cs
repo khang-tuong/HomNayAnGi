7cb7592edcfc6ffc142a76d39fae69f98b7a52dd
khang-tuong <jakelong1232007@gmail.com>
2017-04-21 21:58:49 +0700

    Add project files.

---
+++ HomNayAnGi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace HomNayAnGi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ HomNayAnGi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HomNayAnGi.WebApiApplication" Language="C#" %>
